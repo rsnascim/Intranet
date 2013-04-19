159dbe71b9a408929bf2ac5c6cddac3e78dd0a64
Roberta Schimidt do Nascimento <beta_schimidt@hotmail.com>
2013-04-19 12:02:39 -0300

    Commit 19/04/2013 - Prog 117

--- views/T0117/js.IncluirExec.php
+++ views/T0117/js.IncluirExec.php
@@ -18,23 +18,27 @@ if ($_REQUEST['cod'] == 2) {
  $login      =  $_REQUEST["login"];  
  
  $tabela     =   "T004_T113";
+ 
+ $funcRM =0;
+ foreach ($obj->retornaFuncionariosRM($login) as $cps => $val) {
+     $funcRM++;
+ } 
+
+if($funcRM == 0){
+    
+    $tabelaFuncRM   =    "T004_T009";
+    $camposFuncRM   =    array("T004_Login"     =>  $login
+                              ,"T009_codigo"    =>  57     );
+    
+    $obj->inserir($tabelaFuncRM, $camposFuncRM);
+    
+}
         
         $user       =   $login;
-        $dadosUser  =   $obj->retornaDadosUsuario($user);
         
-        foreach($dadosUser as $cp   =>  $vl)
-        {
-            $nomeUsuario    =   $vl['NomeUsuario'];
-            $emailUsuario   =   $vl['EmailUsuario'];
-        }
-                
         $campos =   array(
                             "T113_codigo"           =>  $codRM
                          ,  "T004_login"            =>  $login
-                         ,  "T004_T113_nome"        =>  $nomeUsuario
-                         ,  "T004_T113_email"       =>  $emailUsuario
-                         ,  "T004_T113_telefone"    =>  ""
-                         ,  "T004_T113_notificado"  =>  ""
                          ,  "T004_T113_tipo"        =>  2               //Tipo 1 = Responsaveis RM
                          );
 
@@ -44,24 +48,29 @@ $obj->inserir($tabela, $campos);
             
         $codRM      =  $_REQUEST["codRM"];  
         $login      =  $_REQUEST["login"];
+        
+         $funcRM =0;
+        foreach ($obj->retornaFuncionariosRM($login) as $cps => $val) {
+            $funcRM++;
+        } 
+
+       if($funcRM == 0){
+
+           $tabelaFuncRM   =    "T004_T009";
+           $camposFuncRM   =    array("T004_Login"     =>  $login
+                                     ,"T009_codigo"    =>  57     );
+
+           $obj->inserir($tabelaFuncRM, $camposFuncRM);
+
+       }
             
         $tabela     =   "T004_T113";
         
         $user       =   $login;
-        $dadosUser  =   $obj->retornaDadosUsuario($user);
         
-        foreach($dadosUser as $cp   =>  $vl)
-        {
-            $nomeUsuario    =   $vl['NomeUsuario'];
-            $emailUsuario   =   $vl['EmailUsuario'];
-        }
-                
         $campos =   array(
                             "T113_codigo"           =>  $codRM
                          ,  "T004_login"            =>  $login
-                         ,  "T004_T113_nome"        =>  $nomeUsuario
-                         ,  "T004_T113_email"       =>  $emailUsuario
-                         ,  "T004_T113_telefone"    =>  ""
                          ,  "T004_T113_tipo"        =>  1               //Tipo 1 = Responsaveis RM
                          );
         
@@ -70,7 +79,7 @@ $obj->inserir($tabela, $campos);
        
        } elseif($_REQUEST["cod"]    ==  3){
            
-            $tabela     =   "T004_T113";
+            $tabela     =   "T119_executores_externos";
             $codRM      =  $_REQUEST["codRM"];  
             $nome       =  $_REQUEST["nome"];
             $tel        =  $_REQUEST["tel"];
@@ -79,12 +88,11 @@ $obj->inserir($tabela, $campos);
             
         
         $campos =   array(
-                            "T113_codigo"           =>  $codRM
-                         ,  "T004_T113_nome"        =>  $nome
-                         ,  "T004_T113_email"       =>  $email
-                         ,  "T004_T113_telefone"    =>  $tel
-                         ,  "T004_T113_notificado"  =>  $notif
-                         ,  "T004_T113_tipo"        =>  3               //Tipo 3 = Responsaveis RM Externo
+                            "T113_codigo"      =>  $codRM
+                         ,  "T119_nome"        =>  $nome
+                         ,  "T119_email"       =>  $email
+                         ,  "T119_telefone"    =>  $tel
+                         ,  "T119_notificado"  =>  $notif
                          );
         
        
@@ -92,29 +100,19 @@ $obj->inserir($tabela, $campos);
            
        } elseif ($_REQUEST["cod"]   ==  4) {
        
-           $tabela  =   "T004_T113";
+           $tabela  =   "T113_T118";
            $codRM   =  $_REQUEST["codRM"];
            $login   =  $_REQUEST["login"];
            $parecer =  $_REQUEST["parecer"];
            $aprovar =  $_REQUEST["aprovar"];         
            $user       =   $login;
-        $dadosUser  =   $obj->retornaDadosUsuario($user);
-        
-        foreach($dadosUser as $cp   =>  $vl)
-        {
-            $nomeUsuario    =   $vl['NomeUsuario'];
-            $emailUsuario   =   $vl['EmailUsuario'];
-        }
-                
+  
         $campos =   array(
                             "T113_codigo"               =>  $codRM
                          ,  "T004_login"                =>  $login
-                         ,  "T004_T113_nome"            =>  $nomeUsuario
-                         ,  "T004_T113_email"           =>  $emailUsuario
-                         ,  "T004_T113_telefone"        =>  ""
-                         ,  "T004_T113_tipo"            =>  4        
-                         ,  "T004_T113_justificativa"   => $parecer   //Tipo 1 = Responsaveis RM
-                         ,  "T004_T113_aprovado"        => $aprovar       
+                         ,  "T113_T118_voto"            =>  $aprovar
+                         ,  "T113_T118_parecer"         => $parecer   
+                         ,  "T113_T118_data"            => date("Y-m-d")
                          );
         
   
